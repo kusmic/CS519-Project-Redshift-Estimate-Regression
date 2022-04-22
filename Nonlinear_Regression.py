13931d4f5462651e0332790a41a80ba2465176fc
hmirh <98181658+hmirh@users.noreply.github.com>
2022-04-22 17:54:45 -0600

    Update Nonlinear_Regression.py

--- Nonlinear_Regression.py
+++ Nonlinear_Regression.py
@@ -1,11 +1,14 @@
 
+
 """
 Created on Tue Mar 29 11:12:19 2022
 @author: Hengameh
 """
 
-import pandas as pd
 import numpy as np
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.preprocessing import StandardScaler
@@ -18,8 +21,16 @@ import time
 data = pd.read_csv('mldata_log_est.csv')
 #data = pd.read_csv('mldata_lin_est.csv')
 x = data.iloc[:,:-1].values
-y = data['redshift'].values
-y = y.reshape(y.shape[0],1)
+y1 = data['redshift'].values
+y = y1.reshape(y1.shape[0],1)
+
+"""correlation_matrix = np.corrcoef(x.T,y1)
+features_y = ['redshift']
+features_x = ['F606W','F814W','F125W','F160W','F606W-F814W','F814W-F125W','F125W-F160W']
+features = features_x + features_y
+
+sns.heatmap(correlation_matrix, annot = True, yticklabels = features, xticklabels = features)
+plt.show()"""
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=1)
 
@@ -33,7 +44,7 @@ y_train_std = sc1.transform(y_train)
 y_test_std = sc1.transform(y_test)
 
 #__________________________________________________________________
-twoD = PolynomialFeatures(degree=2)
+"""twoD = PolynomialFeatures(degree=2)
 x_train_twoD_std = twoD.fit_transform(x_train_std)
 x_test_twoD_std = twoD.fit_transform(x_test_std)
 
@@ -42,10 +53,10 @@ non_reg = LinearRegression()
 non_reg.fit(x_train_twoD_std, y_train_std)
 print(" Running time: %s seconds " % (time.time() - start_time))
 y_test_pred_std = non_reg.predict(x_test_twoD_std)
-y_train_pred_std = non_reg.predict(x_train_twoD_std)
+y_train_pred_std = non_reg.predict(x_train_twoD_std)"""
 #___________________________________________________________________
 
-"""threeD = PolynomialFeatures(degree=3)
+threeD = PolynomialFeatures(degree=3)
 x_train_threeD_std = threeD.fit_transform(x_train_std)
 x_test_threeD_std = threeD.fit_transform(x_test_std)
 
@@ -54,7 +65,7 @@ non_reg = LinearRegression()
 non_reg.fit(x_train_threeD_std, y_train_std)
 print("Running time: %s seconds " % (time.time() - start_time))
 y_test_pred_std = non_reg.predict(x_test_threeD_std)
-y_train_pred_std = non_reg.predict(x_train_threeD_std)"""
+y_train_pred_std = non_reg.predict(x_train_threeD_std)
 
 #_____________________________________________________________________
 
@@ -63,8 +74,8 @@ y_train_pred = sc1.inverse_transform(y_train_pred_std)
 
 error_train = mean_squared_error(y_train, y_train_pred)
 error_test = mean_squared_error(y_test, y_test_pred)
-print("Error of training data", error_train)
-print("Error of testing data", error_test)
+print("MSE of training data", error_train)
+print("MSE of testing data", error_test)
 
 r2_train = r2_score (y_train, y_train_pred)
 r2_test = r2_score (y_test, y_test_pred)
